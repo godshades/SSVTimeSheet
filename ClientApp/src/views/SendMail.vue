4ccd69d3b7e19457fb2c5780c04807efb4be019b
VinhNC <vinhnc@saisystem.vn>
2020-03-11 17:56:44 +0700

    Code controller

--- ClientApp/src/views/SendMail.vue
+++ ClientApp/src/views/SendMail.vue
@@ -1,56 +1,61 @@
 ﻿<template>
   <div class="send-mail">
-    <div class="container">
+    <div class="container" style="text-align:left">
       <div class="py-5 text-center">
         <img v-bind:src="img_logo" style="width:50px" />
         <h4 class="mb-3">Mẫu gửi báo cáo hằng ngày</h4>
       </div>
       <div class="row">
+        <div class="col-md-12 mb-3">
+          <p v-if="errors.length">
+            <ul>
+              <li style="color:red" v-for="error in errors">{{ error }}</li>
+            </ul>
+          </p>
+        </div>
         <div class="col-md-6 mb-3">
           <label for="firstName">Tên :</label>
           <div class="input-group">
             <div class="input-group-prepend">
               <span class="input-group-text"></span>
             </div>
-            <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
+            <input type="text" class="form-control" v-model="usersname" />
           </div>
         </div>
         <div class="col-md-6 mb-3">
           <label for="lastName">Địa chỉ mail :</label>
-          <input type="text" class="form-control" id="lastName" placeholder="" value="" required readonly>
+          <input type="text" class="form-control" v-model="mail" readonly />
         </div>
         <div class="col-md-12 mb-3">
           <label for="username">Mail người nhận :</label>
-          <input type="text" class="form-control" id="username" placeholder="" required>
+          <input type="text" class="form-control" v-model="mailsender" />
         </div>
         <div class="col-md-12 mb-3">
-          <label for="email">Nội dung công việc hôm nay :<span class="text-muted"></span></label>
-          <input type="email" class="form-control" id="email" placeholder="" required>
-        </div>
-        <div class="col-md-9 mb-3">
-          <label for="address">Tiến độ công việc <span class="text-muted">(Task-Percent)</span> :</label>
-          <div class="input-group">
-            <div class="input-group-prepend">
-              <span class="input-group-text"></span>
-            </div>
-            <input type="text" class="form-control" id="address2" placeholder="" required>
-          </div>
-        </div>
-        <div class="col-md-3 mb-3">
-          <span class="btn btn-success" id="btnAdd">Thêm</span>
-          <span class="btn btn-danger" id="btnDel">Xóa</span>
+          <label for="username">Mật khẩu - bảo mật cao nhé :))</label>
+          <input type="password" class="form-control"/>
         </div>
         <div class="col-md-12 mb-3">
-          <label for="username">Mail người nhận :</label>
-          <input type="text" class="form-control" id="username" placeholder="" required>
+          <label for="email">Nội dung công việc hôm nay :</label>
+          <input type="email" class="form-control" v-model="content" />
         </div>
         <div class="col-md-12 mb-3">
-          <label for="address2">Dự kiến kế hoạch tiếp theo : </label>
-          <input type="text" class="form-control" id="address2" placeholder="" required>
+          <label for="address">
+            Tiến độ công việc
+            <span class="text-muted">(Task-Percent)</span> :
+          </label>
+          <input type="text" class="form-control" v-model="task" />
+        </div>
+        <div class="col-md-12 mb-3">
+          <label for="username">Vấn đề phát sinh :</label>
+          <input type="text" class="form-control" v-model="err" />
+        </div>
+        <div class="col-md-12 mb-3">
+          <label for="address2">Dự kiến kế hoạch tiếp theo :</label>
+          <input type="text" class="form-control" v-model="contentTm" />
         </div>
       </div>
       <div class="form-group text-center">
-        <button class="btn btn-success" type="submit" v-on:click="sendMail">Gửi mail</button>
+        <button class="btn btn-success" type="submit" @click="sendMail">Gửi mail</button>
       </div>
     </div>
   </div>
@@ -58,17 +63,61 @@
 
 <script>
   import axios from 'axios'
+
   export default {
+    name: 'send-mail',
     data() {
       return {
-        img_logo: 'https://timviec365.vn/pictures/2019/11/27/avatar185497.jpg'
+        errors: [],
+        img_logo: 'https://timviec365.vn/pictures/2019/11/27/avatar185497.jpg',
+        usersname: '',
+        mail: '',
+        mailsender: '',
+        content: '',
+        task: '',
+        err: '',
+        contentTm: ''
       }
     },
     methods: {
       sendMail: function () {
+        this.errors = [];
+
+        if (!this.usersname) {
+          this.errors.push('Tên chưa nhập ');
+        }
+        if (!this.mail) {
+          this.errors.push('Mail chưa nhập ');
+        }
+        if (!this.mailsender) {
+          this.errors.push('Mail người nhận chưa nhập ');
+        }
+        if (!this.content) {
+          this.errors.push('Nội dung chưa nhập ');
+        }
+        if (!this.task) {
+          this.errors.push('Tiến độ công việc chưa nhập ');
+        }
+        if (!this.contentTm) {
+          this.errors.push('Kế hoạch tiếp theo chưa nhập ');
+        }
+        if (this.usersname && this.mail && this.mailsender && this.content && this.task && this.contentTm) {
+          var users = JSON.stringify({
+          Name: this.usersname,
+          Mail: this.mail,
+          Mailsender: this.mailsender,
+          Content: this.content,
+          Percent: this.task,
+          Err: this.err,
+          Tmcontent: this.contentTm
+        });
         axios({
           method: 'post',
           url: '/sendMail/sendDataToMail',
+          data: users,
+          headers: {
+            'Content-Type': 'application/json'
+          }
         })
           .then(function (response) {
             console.log(response);
@@ -76,7 +125,9 @@
           .catch(function (error) {
             console.log(error);
           });
+        } 
       }
     }
   }
+
 </script>
--- Controllers/SendMailController.cs
+++ Controllers/SendMailController.cs
@@ -1,24 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace SSVTimeSheet.Controllers
 {
     public class SendMailController : Controller
     {
         [HttpPost]
-        public IActionResult SendDataToMail(ReportEmployee e)
+        public async Task<IActionResult> SendDataToMail([FromBody]ReportEmployee report)
         {
-            int a = e.ReportId;
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("Dear A.Hùng@@");
+            sb.Append("Báo cáo công việc trong ngày hôm nay@@");
+            sb.Append("1) Nội dung công việc thực hiện ngày hôm nay:@");
+            sb.Append("   - " + report.Content);
+            sb.Append("2) Mức độ hoàn thành công việc (%):@");
+            sb.Append("   - " + report.Percent);
+            sb.Append("3) Vấn đề phát sinh:@");
+            sb.Append("   - " + report.Err);
+            sb.Append("4) Kế hoạch tiếp theo:@");
+            sb.Append("   - " + report.Tmcontent);
+            sb.Append("Nhờ anh xác nhận giúp em. Em xin cảm ơn!");
+
+            sb = sb.Replace("@", Environment.NewLine);
+
+            try
+            {
+                SmtpClient client = new SmtpClient("saisystem.vn", 587);
+                client.EnableSsl = true;
+                client.Credentials = new NetworkCredential("vinhnc@saisystem.vn", "20150601");
+
+                MailMessage message = new MailMessage("vinhnc@saisystem.vn", "quyennq@saisystem.vn");
+                message.Subject = "Báo cáo công việc";
+                message.Body = sb.ToString();
+
+                client.Send(message);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
 
             return View();
         }
 
         public class ReportEmployee
         {
-            public int ReportId { get; set; }
+            public string Name { get; set; }
+
+            public string Mail { get; set; }
+
+            public string Mailsender { get; set; }
+
+            public string Content { get; set; }
+
+            public string Percent { get; set; }
+
+            public string Err { get; set; }
+
+            public string Tmcontent { get; set; }
         }
     }
 }
